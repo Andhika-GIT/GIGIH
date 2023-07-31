a74cdf1e11e049e37cdfa9ad82f233741f71eed4
Andhika-GIT <andhikapr081@gmail.com>
2023-07-31 21:28:17 +0700

    spotify-react setup the search UI preparing the useFetch custom hooks

--- spotify-react/src/App.jsx
+++ spotify-react/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 // components
-import Button from "./components/Button";
-import Spotify from "./components/Spotify";
-import HandsOn from "./components/HandsOn";
+import Button from './components/Button';
+import Spotify from './components/Spotify';
+import HandsOn from './components/HandsOn';
 
 function App() {
   return (
     <>
-      <HandsOn />
+      <Spotify />
     </>
   );
 }
--- spotify-react/src/components/Spotify/index.jsx
+++ spotify-react/src/components/Spotify/index.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import AllSongs from "./AllSongs";
-import FavoriteSongs from "./FavoriteSongs";
+import React from 'react';
+import AllSongs from './AllSongs';
+import FavoriteSongs from './FavoriteSongs';
 
 const Spotify = () => {
   return (
     <>
-      <h1 style={{ color: "#1DB954" }}>SpotSenja</h1>
-      <h3>All Songs</h3>
+      <h1 style={{ color: '#1DB954' }}>SpotSenja</h1>
+      <div className="search-field">
+        <label htmlFor="">Search songs</label>
+        <input type="text" />
+      </div>
+
       <AllSongs />
-      <h3>Favorite Songs</h3>
-      <FavoriteSongs />
+      {/* <h3>Favorite Songs</h3>
+      <FavoriteSongs /> */}
     </>
   );
 };
